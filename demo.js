e041763db7f8be416e41238708778c34e630a84d
Dana Johnson <danajohnson@Danas-iMac.local>
2014-03-25 22:14:48 -0700

    laptop

--- demo.js
+++ demo.js
@@ -1,56 +1,97 @@
 function profile()
 {
 	$('#profile_content').siblings().hide();
-    $('#profile_content').show();
+    $('#profile_content').fadeIn();
 }
 
 function newsfeed()
 {
 	$('#newsfeed_content').siblings().hide();
-    $('#newsfeed_content').show();
+    $('#newsfeed_content').fadeIn();
 }
 
 function courses()
 {
     $('#courses_content').siblings().hide();
-    $('#courses_content').show();
+    $('#courses_content').fadeIn();
 }
 
 function stats()
 {
     $('#stats_content').siblings().hide();
-    $('#stats_content').show();
+    $('#stats_content').fadeIn();
 }
 
 function friends()
 {
 	$('#friends_content').siblings().hide();
-    $('#friends_content').show();
+    $('#friends_content').fadeIn();
 }
 
 function challenges()
 {
 	$('#challenges_content').siblings().hide();
-    $('#challenges_content').show();
+    $('#challenges_content').fadeIn();
 }
 
 function achievements()
 {
 	$('#achievements_content').siblings().hide();
-    $('#achievements_content').show();
+    $('#achievements_content').fadeIn();
 }
 
 function help()
 {
 	$('#help_content').siblings().hide();
-    $('#help_content').show();
+    $('#help_content').fadeIn();
 }
 
-
 $(document).ready(function() {
+	// PROFILE
     $('#profile').click(function() {
     	profile();
     });
+
+    // *** name ***
+    $('#name_edit').click(function() { // open edit input
+    	$('#name_input').val("");
+    	$('#name_form').toggle();
+    });
+    $('#name_form').submit(function(event) { // submit input
+    	event.preventDefault();
+    	var edit = $('#name_input').val();
+    	console.log(edit);
+    	if (edit !== "") {
+    		$('#name_h2').text(edit);
+    	}
+    	$('#name_form').toggle();
+    });
+    $('#name_cancel').click(function() { // cancel input
+    	$('#name_form').toggle();
+    });
+
+    // *** bio ***
+    $('#bio_edit').click(function() { // open edit textarea
+    	$('#bio_text').val("");
+    	$('#bio_form').toggle();
+    });
+    $('#bio_form').submit(function(event) { // submit textarea
+    	event.preventDefault();
+    	var edit = $('#bio_text').val().split("\n");
+    	console.log(edit);
+    	if (edit !== "") {
+    		$('#bio_div').empty();
+    		for (var i = 0; i < edit.length; i++) {
+    			$('#bio_div').append("<p>" + edit[i] + "</p>");
+    		};
+    	}
+    	$('#bio_form').toggle();
+    });
+    $('#bio_cancel').click(function() { // cancel textarea
+    	$('#bio_form').toggle();
+    });
+
+    // NEWSFEED
     $('#newsfeed').click(function() {
     	newsfeed();
     });
