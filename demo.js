1a0aaf899d2b19de8477460376dfce638b6c45a4
Dana Johnson <danajohnson@Danas-iMac.local>
2014-03-25 23:57:25 -0700

    desktop

--- demo.js
+++ demo.js
@@ -1,97 +1,186 @@
+// ------------------------------------------------------------------------ TABS
 function profile()
 {
 	$('#profile_content').siblings().hide();
-    $('#profile_content').fadeIn();
+    $('#profile_content').fadeIn('fast');
 }
 
 function newsfeed()
 {
 	$('#newsfeed_content').siblings().hide();
-    $('#newsfeed_content').fadeIn();
+    $('#newsfeed_content').fadeIn('fast');
 }
 
 function courses()
 {
     $('#courses_content').siblings().hide();
-    $('#courses_content').fadeIn();
+    $('#courses_content').fadeIn('fast');
 }
 
 function stats()
 {
     $('#stats_content').siblings().hide();
-    $('#stats_content').fadeIn();
+    $('#stats_content').fadeIn('fast');
 }
 
 function friends()
 {
 	$('#friends_content').siblings().hide();
-    $('#friends_content').fadeIn();
+    $('#friends_content').fadeIn('fast');
 }
 
 function challenges()
 {
 	$('#challenges_content').siblings().hide();
-    $('#challenges_content').fadeIn();
+    $('#challenges_content').fadeIn('fast');
 }
 
 function achievements()
 {
 	$('#achievements_content').siblings().hide();
-    $('#achievements_content').fadeIn();
+    $('#achievements_content').fadeIn('fast');
 }
 
 function help()
 {
 	$('#help_content').siblings().hide();
-    $('#help_content').fadeIn();
+    $('#help_content').fadeIn('fast');
 }
 
+//------------------------------------------------------------------ NAME EDITOR
+function openNameEditor()
+{
+	$('#name_input').val("");
+    $('#name_form').slideToggle('fast');
+}
+
+function closeNameEditor()
+{
+	$('#name_form').hide();
+}
+
+function submitNameEdit()
+{
+	var edit = $('#name_input').val();
+	console.log(edit);
+	if (edit.length > 0) {
+		$('.name_h2').text(edit);
+	}
+	$('#name_form').hide();
+}
+
+//------------------------------------------------------------------- BIO EDITOR
+function openBioEdtor()
+{
+	$('#bio_text').val("");
+    $('#bio_form').slideToggle('fast');
+}
+
+function closeBioEditor()
+{
+	$('#bio_form').hide();
+}
+
+function submitBioEdit()
+{
+	var edit = $('#bio_text').val().split("\n");
+	console.log(edit);
+	if (edit.length > 0) {
+		$('#bio_div').empty();
+		for (var i = 0; i < edit.length; i++) {
+			$('#bio_div').append("<p>" + edit[i] + "</p>");
+		};
+	}
+	$('#bio_form').hide();
+}
+
+function openQuoteEditor()
+{
+	$('#quote_text').val("");
+	$('#quote_input').val("");
+	$('#quote_form').slideToggle('fast');
+}
+
+function closeQuoteEditor()
+{
+	$('#quote_form').hide();
+}
+
+function submitQuoteEdit()
+{
+	var new_quote = $('#quote_text').val();
+	var new_author = $('#quote_input').val();
+	if (new_quote.length > 0) {
+		new_quote = removeQuotes(new_quote);
+		$('.quote').text("\"" + new_quote + "\"");
+	}
+	if (new_author.length > 0) {
+		$('.quote_author').text(new_author);
+	}
+	$('#quote_form').hide();
+}
+
+//----------------------------------------------------------------- MY UTILITIES
+function removeQuotes(str)
+{
+	var result = str;
+	if (result.charAt(0) === "\"") {
+		result = result.substr(1);
+	}
+	if (result.charAt(result.length - 1) === "\"") {
+		result = result.substr(0,result.length - 1);
+	}
+	return result;
+}
+
+//------------------------------------------------------------------------------
+//
+//                                                                     DOC READY
+//
+//------------------------------------------------------------------------------
 $(document).ready(function() {
-	// PROFILE
+	//-------------------------------------------------------------- PROFILE TAB
     $('#profile').click(function() {
     	profile();
     });
 
-    // *** name ***
+    //--------------------------------------------------------------------- name
     $('#name_edit').click(function() { // open edit input
-    	$('#name_input').val("");
-    	$('#name_form').toggle();
+    	openNameEditor();
     });
     $('#name_form').submit(function(event) { // submit input
     	event.preventDefault();
-    	var edit = $('#name_input').val();
-    	console.log(edit);
-    	if (edit !== "") {
-    		$('#name_h2').text(edit);
-    	}
-    	$('#name_form').toggle();
+    	submitNameEdit();
     });
     $('#name_cancel').click(function() { // cancel input
-    	$('#name_form').toggle();
+    	closeNameEditor();
     });
 
-    // *** bio ***
+    //---------------------------------------------------------------------- bio
     $('#bio_edit').click(function() { // open edit textarea
-    	$('#bio_text').val("");
-    	$('#bio_form').toggle();
+    	openBioEdtor();
     });
     $('#bio_form').submit(function(event) { // submit textarea
     	event.preventDefault();
-    	var edit = $('#bio_text').val().split("\n");
-    	console.log(edit);
-    	if (edit !== "") {
-    		$('#bio_div').empty();
-    		for (var i = 0; i < edit.length; i++) {
-    			$('#bio_div').append("<p>" + edit[i] + "</p>");
-    		};
-    	}
-    	$('#bio_form').toggle();
+    	submitBioEdit();
     });
     $('#bio_cancel').click(function() { // cancel textarea
-    	$('#bio_form').toggle();
+    	closeBioEditor();
     });
 
-    // NEWSFEED
+    //-------------------------------------------------------------------- quote
+	$('#quote_edit').click(function() { // open edit textarea
+    	openQuoteEditor();
+    });
+    $('#quote_form').submit(function(event) { // submit textarea
+    	event.preventDefault();
+    	submitQuoteEdit();
+    });
+    $('#quote_cancel').click(function() { // cancel textarea
+    	closeQuoteEditor();
+    });
+
+    //----------------------------------------------------------------- NEWSFEED
     $('#newsfeed').click(function() {
     	newsfeed();
     });
